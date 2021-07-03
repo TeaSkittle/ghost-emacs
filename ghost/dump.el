47c716f0656f945eb4fbb0f978d92dcec5b43205
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2021-07-03 10:43:41 -0600

    Update dump.el

--- ghost/dump.el
+++ ghost/dump.el
@@ -1,19 +1,1 @@
 ;; Space for emacs customize to dump its data, no need to edit this file
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" default)))
- '(package-selected-packages
-   (quote
-    (dashboard monokai-theme use-package rainbow-delimiters naysayer-theme markdown-mode fish-mode d-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
